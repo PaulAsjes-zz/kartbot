9cf0799d42f5cbb74c1d8c3f03a79e90941036e5
Paul Asjes <p.asjes@gmail.com>
2015-10-30 15:17:45 -0400

    now using template strings

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function kartbot(opts) {
   var pool;
 
   slack.on('open', function() {
-    console.log('Connected to ' + slack.team.name + ' as ' + slack.self.name);
+    console.log(`Connected to ${slack.team.name} as ${slack.self.name}`);
     var channels = getChannels(slack.channels);
-    console.log('Currently in: ' + channels.join(', '));
+    console.log(`Currently in: ${channels.join(', ')}`);
   });
 
   slack.on('message', function(message) {
@@ -41,6 +41,7 @@ function kartbot(opts) {
     if (message.text) {
       var msg = message.text.toLowerCase();
       var args = msg.split(' ');
+
       switch (true) {
         // say hi!
         case (msg.indexOf('hi kartbot') > -1):
@@ -54,7 +55,7 @@ function kartbot(opts) {
 
           var res = responses[Math.floor(Math.random() * responses.length)];
 
-          channel.send('Hi ' + upper(user.name) + res);
+          channel.send(`Hi ${upper(user.name)} ${res}`);
 
           break;
 
@@ -64,32 +65,36 @@ function kartbot(opts) {
           break;
 
         // challenge other members to game of smash
-        case (args[0] === '!smash':
+        case (args[0] === '!smash'):
           pool = challenge(channel, members, user, args, 'Smash');
           break;
 
         // opt out of playing
-        case (args[0] === '!nokart':
+        case (args[0] === '!nokart'):
+          console.log(pool);
           pool = reject(channel, members, user, pool);
+          console.log(pool);
           break;
 
         // show amount of times members have challenged and have been challenged
-        case (args[0] === '!stats':
+        case (args[0] === '!stats'):
 
           break;
 
         // roll the dice against someone else
-        case (args[0] === '!roll':
+        case (args[0] === '!roll'):
           roll(args, user, channel, members);
           break;
 
         // send list of commands
         case (msg.indexOf('!help') > -1):
-          channel.send('Hi ' + upper(user.name) + '! Possible commands are: \n ' +
-                       '\`!kart\` - Challenge random channel members to Mario Kart \n ' +
-                       '\`!smash\` - Challenge random channel members to Smash Bros \n ' +
-                       '\`!nokart\` - Reject the challenge :( \n ' +
-                       '\`!roll USER\` - Challenge someone in the channel to a game of chance');
+          channel.send(
+            `Hi ${upper(user.name)}! Possible commands are:
+            \`!kart\` - Challenge random channel members to Mario Kart
+            \`!smash\` - Challenge random channel members to Smash Bros
+            \`!nokart\` - Reject the challenge :(
+            \`!roll USER\` - Challenge someone in the channel to a game of chance`
+         );
           break;
       }
     }
@@ -105,7 +110,7 @@ function kartbot(opts) {
 function roll(args, user, channel, members) {
   if (members.indexOf(args[1]) > -1) {
     if (args[1] === user.name) {
-      channel.send(upper(user.name) + ' tried to roll against themselves. _They lost._');
+      channel.send(`${upper(user.name)} tried to roll against themselves. _They lost._`);
       return;
     }
 
@@ -116,17 +121,23 @@ function roll(args, user, channel, members) {
 
     var winner = firstRoll > secondRoll ? c : o;
 
-    channel.send(c + ' fancies their chances against ' + o + '!\n' + c + ' rolls: ' + firstRoll + '\n' + o + ' rolls: ' + secondRoll + '\n\n*' + winner + ' is the winner!*');
+    channel.send(`${c} fancies their chances against ${o}!\n${c} rolls: ${firstRoll}\n${o} rolls: ${secondRoll}\n\n*${winner} is the winner!*`);
   } else {
     // stop people from trying !roll without a command
     var joined = args.slice(1).join(' ');
     var res = args.slice(1).length > 1 || joined === '' ? 'that dumb thing you just tried' : joined;
-    channel.send('Sorry ' + upper(user.name) + ', but ' + res + ' is in another castle.');
+    channel.send(`Sorry ${upper(user.name)}, but ${res} is in another castle.`);
   }
 }
 
 function reject(channel, members, user, pool) {
   if (pool.indexOf(user.name) > 0) {
+    // if the challenger drops out, cancel the whole thing
+    if (pool[pool.length - 1] === user.name) {
+      channel.send(upper(`${user.name} dropped out, challenge has been cancelled!`));
+      return pool = [];
+    }
+
     // remove original rejecter and kartbot
     members.splice(pool.indexOf(user.name), 1);
     members.splice(pool.indexOf('kartbot'), 1);
@@ -136,14 +147,15 @@ function reject(channel, members, user, pool) {
       newPlayer = members[Math.floor(Math.random() * members.length)];
     } while(pool.indexOf(newPlayer) > -1)
 
-    channel.send(upper(user.name) + ' has dropped out! ' + upper(newPlayer) + ' has been challenged in their place!');
+    channel.send(`${upper(user.name)} has dropped out! ${upper(newPlayer)} has been challenged in their place!`);
 
     pool.splice(pool.indexOf(user.name), 1, newPlayer);
     var karters = pool.map(upper);
-    channel.send('Current karters: ' + karters.join(', '));
+    channel.send(`Current karters: ${karters.join(', ')}`);
   } else {
-    channel.send(upper(user.name) + ' has tried to drop out, but they weren\'t invited to play! Jerk!');
+    channel.send(`${upper(user.name)} has tried to drop out, but they weren\'t invited to play! Jerk!`);
   }
+
   return pool;
 }
 
@@ -152,7 +164,7 @@ function challenge(channel, members, user, args, game) {
 
   for (var i = 1; i < args.length; i++) {
     if (members.indexOf(args[i]) < 0) {
-      channel.send(upper(user.name) + ' wants a game of ' + game + ', but I can\'t find ' + args[i] + ' in this channel!');
+      channel.send(`${upper(user.name)} wants a game of ${game}, but I can\'t find ${args[i]} in this channel!`);
       return;
     }
   }
@@ -188,11 +200,11 @@ function challenge(channel, members, user, args, game) {
   }
 
   if (pool.length < (maxPlayers - 1)) {
-    ret = ' Room for ' + ((maxPlayers - 1) - pool.length) + ' more!';
+    ret = ` Room for ${(maxPlayers - 1) - pool.length} more!`;
   }
 
   var names = pool.map(upper);
-  channel.send(game + ' time! ' + upper(user.name) + ' has challenged ' + names.join(', ') + ' to a game of ' + game + '!' + ret);
+  channel.send(`${game} time! ${upper(user.name)} has challenged ${names.join(', ')} to a game of ${game}! ${ret}`);
 
   // add the original challenger back to the list
   pool.push(user.name);
